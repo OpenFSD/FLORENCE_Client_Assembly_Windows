841e9ac337a967de025a491e688a9b0f28aa5eb9
Brenton Maddocks <Legelowes@gmail.com>
2024-11-28 14:01:35 +1000

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,108 @@
+﻿using FLORENCE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using Valve.Sockets;
+
+namespace FLORENCE
+{
+    public class Client
+    {
+        public Client()
+        {
+            NetworkingSockets client = new NetworkingSockets();
+
+            uint connection = 0;
+
+            StatusCallback status = (ref StatusInfo info) => {
+                switch (info.connectionInfo.state)
+                {
+                    case ConnectionState.None:
+                        break;
+
+                    case ConnectionState.Connected:
+                        Console.WriteLine("Client connected to server - ID: " + connection);
+                        break;
+
+                    case ConnectionState.ClosedByPeer:
+                    case ConnectionState.ProblemDetectedLocally:
+                        client.CloseConnection(connection);
+                        Console.WriteLine("Client disconnected from server");
+                        break;
+                }
+            };
+
+            NetworkingUtils utils = new NetworkingUtils();
+            utils.SetStatusCallback(status);
+
+            Address address = new Address();
+
+            address.SetAddress("::1", port);
+
+            connection = client.Connect(ref address);
+
+#if VALVESOCKETS_SPAN
+	MessageCallback message = (in NetworkingMessage netMessage) => {
+		Console.WriteLine("Message received from server - Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
+	};
+#else
+            const int maxMessages = 20;
+
+            NetworkingMessage[] netMessages = new NetworkingMessage[maxMessages];
+#endif
+
+            while (!Console.KeyAvailable)
+            {
+                client.RunCallbacks();
+
+#if VALVESOCKETS_SPAN
+		client.ReceiveMessagesOnConnection(connection, message, 20);
+#else
+                int netMessagesCount = client.ReceiveMessagesOnConnection(connection, netMessages, maxMessages);
+
+                if (netMessagesCount > 0)
+                {
+                    for (int i = 0; i < netMessagesCount; i++)
+                    {
+                        ref NetworkingMessage netMessage = ref netMessages[i];
+
+                        Console.WriteLine("Message received from server - Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
+
+                        netMessage.Destroy();
+                    }
+                }
+#endif
+
+                Thread.Sleep(15);
+            }
+        }
+
+        public static void CreateAndSendNewMessage()
+        {
+            byte[] data = new byte[64];
+            sockets.SendMessageToConnection(connection, data);
+        }
+
+        public static void CopyPayloadFromMessage()
+        {
+            byte[] buffer = new byte[1024];
+            netMessage.CopyTo(buffer);
+        }
+
+        public static void SetA_HookForDebugInformation()
+        {
+            DebugCallback debug = (type, message) => {
+                Console.WriteLine("Debug - Type: " + type + ", Message: " + message);
+            };
+
+            NetworkingUtils utils = new NetworkingUtils();
+
+            utils.SetDebugCallback(DebugType.Everything, debug);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FLORENCE
+{
+    public class Program
+    {
+        //private static FLORENCE.framework framework;
+        private static FLORENCE.Client networkingClient;
+
+        public static void Main(String[] args)
+        {
+            System.Console.WriteLine("FLORENCE START");
+
+            //framework = new FLORENCE.framework();
+            //while (framework == null) { /* wait untill created */ }
+
+            Valve.Sockets.Library.Initialize();
+            networkingClient = new FLORENCE.Client();
+        }
+
+        //public static FLORENCE.framework Get_Framework()
+        //{
+        //    return framework;
+        //}
+    }
+}
